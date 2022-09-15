dad121dd49d6b91c54fd4c38224d5c3604c23df8
Olarotimi Adamson <olarotimiadamson@yahoo.com>
2022-09-15 14:35:23 +0100

    props and Mapping added

---
+++ src/Components/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Footer from "./Footer";
+import Note from "./Note";
+import Header from "./Header";
+import notes from "../notes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map((noteDetails) => (
+        <Note
+          key={noteDetails.id}
+          title={noteDetails.title}
+          content={noteDetails.content}
+        />
+      ))}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
